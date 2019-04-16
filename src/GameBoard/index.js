79a8c84f9bcad9d7972b4f44fc4c8953ad4ec2fa
Barry Ross <barry@4d-media.ca>
2019-04-15 23:54:32 -0700

    Adds moves and inversion counter to GameBoard

--- src/GameBoard/index.js
+++ src/GameBoard/index.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
 import shuffle from '../utilities/shuffle'
-import solvability, { findIdx, findPosition } from '../utilities/solvability'
+import solvability, { countInversions, findIdx, findPosition } from '../utilities/solvability'
 import GamePiece from './GamePiece'
 import './styles.scss'
 class GameBoard extends Component {
 
 	state = {
 		pieces: [],
-		count:0
+		moves:0,
+		inversions:null
 	}
 
 	componentDidMount = () => {
@@ -15,29 +16,32 @@ class GameBoard extends Component {
 		while (!solvability(tiles)){
 			tiles = this.generateTiles()
 		}
-		this.setState({pieces:tiles})
-
+		this.setState({pieces:tiles, inversions:countInversions(tiles)})
 	}
   
 	generateTiles = () => {
 		let tiles  = [...Array(16).keys()]
-		return shuffle(tiles)
+		tiles =  shuffle(tiles)
+		return tiles
 	}
 
 	handleGamePieceClick = (num) => {
 		const { pieces } = this.state
-		// Get location of clicked piece
+		this.setState(state =>{
+			return {
+				moves:state.moves+1,
+				inversions: countInversions(pieces)
+			}
+		})
+
 		let pidx = findIdx(pieces, num)
 		let zidx = findIdx(pieces, 0)
 		let p = findPosition(pieces, pidx)
 		let z = findPosition(pieces, zidx)
-		// console.log("location of", num, p)
-		// console.log("location of 0", z)
+
 		if(p.y === z.y && Math.abs(p.x - z.x) === 1){
-			// console.log("should swap with 0, case 1")
 			this.swapPieces(pieces, pidx, zidx)
 		}else if(p.x === z.x && (Math.abs(p.y - z.y) === 1)){
-			// console.log("should swap with 0, case 2")
 			this.swapPieces(pieces, pidx, zidx)
 		}else{
 			console.log("no go!");
@@ -52,32 +56,11 @@ class GameBoard extends Component {
 		this.setState({pieces:array})
 	}
 
-	/*
-		State tracks:
-			- number of moves
-			- the actual moves
-			- current location of pieces on the gameboard
-			- 
-
-		Methods
-			- movePiece
-			- newGame/scramble
-
-
-		Initialization
-			- queries api for an image
-			- creates tiles
-			- scrambles tiles
-			- checks if puzzle is solveable
-
-	*/
-
-
 	render(){
-		const { pieces } = this.state
+		const { pieces, moves, inversions } = this.state
 		return(
-			<section>
-				<h1>15 Puzzle</h1>
+			<section className="GameBoard-container">
+				<header>Moves : {moves}  Inversions: {inversions} </header>
 				<div className="GameBoard">
 					{ pieces.map( piece => 	<GamePiece onClick={this.handleGamePieceClick} key={piece} num={piece}/> )}
 				</div>
