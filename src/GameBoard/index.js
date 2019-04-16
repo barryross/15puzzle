9d1ecafde2233e85d61b66030bbbd2693d9cde66
Barry Ross <barry@4d-media.ca>
2019-04-15 20:37:28 -0700

    Adds GameBoard component with some pseudocode for state and bootstrapping process

---
+++ src/GameBoard/index.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+
+class GameBoard extends Component {
+
+	/*
+		State tracks:
+			- number of moves
+			- the actual moves
+			- current location of pieces on the gameboard
+			- 
+
+		Methods
+			- movePiece
+			- newGame/scramble
+
+
+		Initialization
+			- queries api for an image
+			- creates tiles
+			- scrambles tiles
+			- checks if puzzle is solveable
+
+	*/
+
+	render(){
+		return(
+			<section>
+				<h1>15 Puzzle</h1>
+			</section>
+		)
+	}
+}
+
+export default GameBoard
