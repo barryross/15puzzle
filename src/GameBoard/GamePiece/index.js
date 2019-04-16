05ae7d41ad05ea5eb31c329d8eafc2977c4b5d17
Barry Ross <barry@4d-media.ca>
2019-04-15 22:44:50 -0700

    allows for "blackout" of "0" piece

--- src/GameBoard/GamePiece/index.js
+++ src/GameBoard/GamePiece/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import './styles.scss'
 
-const GamePiece = ({num}) => <div className="GamePiece">{num}</div>
+const GamePiece = ({num}) => <div className={`GamePiece ${num === 0 ? 'blackout' : ""}`}>{num}</div>
 
 GamePiece.propTypes = {
   num: PropTypes.number.isRequired
