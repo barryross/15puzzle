f24bc76ab9ad63ff19e295c1bae0938b636822c0
Barry Ross <barry@4d-media.ca>
2019-04-16 18:54:34 -0700

    refactors solvability utility

--- client/src/utilities/solvability.js
+++ client/src/utilities/solvability.js
@@ -2,25 +2,25 @@
 // Logic for solveability taken from: https://www.defold.com/tutorials/15-puzzle/
 
 /*
-If the empty square is on an odd row (1 or 3 counting from the bottom) and the number of inversions is even.
-If the empty square is on an even row (2 or 4 counting from the bottom) and the number of inversions is odd.
-
+If N is even, puzzle instance is solvable if
+the blank is on an even row counting from the bottom (second-last, fourth-last, etc.) and number of inversions is odd.
+the blank is on an odd row counting from the bottom (last, third-last, fifth-last, etc.) and number of inversions is even.
 */
 
 // Returns true if the order of pieces is solveable
 export default (array) => {
 	let inv = countInversions(array)
-	let idx = findIdx(array, 0) //0 represents empty square
-	let pos = findPosition(array, idx)
+	let idxZero = findIdx(array, 0) //0 represents empty square
+	let posZero = findPosition(array, idxZero)
 
 	console.log("# of inversions", inv)
-	console.log("idx of 5 if ", idx, array)
-	console.log("pos of 5 is ", pos)
+	console.log("idx of 0 is ", idxZero, array)
+	console.log("pos of 0 is ", posZero)
 
-	if (pos.y % 2 === 1 && inv % 2 === 0){
+	if (posZero.y % 2 === 1 && inv % 2 === 0){
 		console.log("solveable!")
 		return true
-	}else if (pos.y % 2 === 0 && inv % 2 === 1){
+	}else if (posZero.y % 2 === 0 && inv % 2 === 1){
 		console.log("solveable!")
 
 		return true
@@ -33,7 +33,7 @@ export default (array) => {
 }
 
 // Returns the number of inversions present
-const countInversions = (array) => {
+export const countInversions = (array) => {
 	let inv = 0;
 	for (let i = 0; i < array.length; i++){ 
 		// For each item, we want to count the number of items with a higher index and lower value
@@ -53,14 +53,14 @@ Indices can be thought of as:
 12 13 14 15
 */
 //for a the following returns the x,y coordinates of the piece on the GameBoard
-const findPosition = (array, idx) => {
+export const findPosition = (array, idx) => {
 	idx++
 	let x, y
 	x = idx - (Math.ceil((idx)/4) - 1) * 4
-	y =  Math.ceil((idx) / 4) //+ 1 compensates for "0"
+	y =  5 - Math.ceil((idx) / 4) //+ 1 compensates for "0"
 	return {x, y}
 }
 
-const findIdx = (array, value) => {
+export const findIdx = (array, value) => {
 		return array.indexOf(value)
 }
