b096181a6e4015a7f79361af46cb228b389ae038
Barry Ross <barry@4d-media.ca>
2019-04-18 18:05:19 -0700

    Cleans up Puzzle

--- client/src/Puzzle/index.js
+++ client/src/Puzzle/index.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react'
-import shuffle from '../utilities/shuffle'
-import {isSolveable, isSwappable, countInversions, findIdx, findPosition } from '../utilities/gameboard'
-import Header from './Header'
-import GameBoard from './GameBoard'
 import axios from 'axios'
+import {isSolveable, isSwappable, countInversions, findIdx, findPosition } from '../utilities/gameboard'
+import PuzzleHeader from './Header'
+import GameBoard from './GameBoard'
+import shuffle from '../utilities/shuffle'
 import './styles.scss'
 
 
@@ -19,31 +19,34 @@ class Puzzle extends Component {
 	}
 
 	 componentDidMount = async () => {
-		// let response =  await axios.get('/api/photos/random')
+		this.initialize()
+	}
+	
+	initialize = async () => {
+		let response =  await axios.get('/api/photos/random')
 
-		//If we received an image from the server
-		// if(response.data.success){
-		// 	const { regular } = response.data.response.urls;
-		// 	this.setState({imgUrl:regular})
-		// }
-		this.setState({imgUrl:"https://picjumbo.com/wp-content/uploads/vineyards-on-italian-coast-free-photo-DSC04256-2210x1473.jpg"})
+		// If we received an image from the server
+		if(response.data.success){
+			const { regular } = response.data.response.urls;
+			this.setState({imgUrl:regular})
+		}
 		let tiles = this.generateTiles();
-		// // while (!isSolveable(tiles)){
-		// // 	tiles = this.generateTiles()
-		// // }
+		//  while (!isSolveable(tiles)){
+		// 	tiles = this.generateTiles()
+		// }
 		this.setState({pieces:tiles, inversions:countInversions(tiles)})
 	}
-  
 	generateTiles = () => {
 		let tiles  = [...Array(16).keys()]
 		tiles =  shuffle(tiles)
-		 tiles = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-		 tiles = [1,2,3,4,5,6,7,8,9,10,0,15,13,14,12,11]
+		tiles = [1,2,3,4,5,6,7,8,9,10,0,15,13,14,12,11]
+
 		return tiles
 	}
 
+
+
 	handleGamePieceClick = (num) => {
-		console.log("NUM", num)
 		const { pieces } = this.state
 		
 		let idxPiece = findIdx(pieces, num)
@@ -75,39 +78,32 @@ class Puzzle extends Component {
 	}
 
 	render(){
+		
 		const { pieces, moves, inversions, imgUrl, referenceImage} = this.state
-		let idxZero = findIdx(pieces, 0)
-		let posZero = findPosition(pieces, idxZero)
-		console.log("DF", this.state.inversions === 0 && posZero.x === 4 && posZero.y ==1)
-		if(this.state.inversions === 0 && posZero.x === 4 && posZero.y ==1){
 			return(
-				<div class="victory">You've won!</div>
-			)
-		}else{
-			return(
-				<section className="Puzzle">hi
-					<Header
+				<section className="Puzzle">
+					<PuzzleHeader
 						toggle={this.toggleReferenceImage}
 						inversions={inversions}
 						moves={moves}
 						referenceImage={referenceImage}
-					/>hi
+					/>
+					
 					<GameBoard
 						imgUrl={imgUrl}
 						pieces={pieces}
+						showModal={inversions}
 						handleGamePieceClick={this.handleGamePieceClick}
 					/>
 					{ referenceImage && 
-							<div 
-							style={{backgroundImage:`url(${imgUrl})`}}
-							className="Reference-container">
-							</div> 
+						<div 
+						 style={{backgroundImage:`url(${imgUrl})`}}
+					  	className="Reference-container">
+						</div> 
 					}
 				</section>
 			)
 		}
-
-	}
 }
 
 export default Puzzle
